c65ed2fe37968d456ff09f81ef4189649a954e79
Eric Ng <EricNg109@gmail.com>
2022-08-24 14:50:05 -0700

    hide api key

--- app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -1,11 +1,12 @@
 package com.udacity.asteroidradar
 
+import com.udacity.asteroidradar.Constants.API_KEY
+
 object Constants {
     const val API_QUERY_DATE_FORMAT = "YYYY-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
-    const val API_KEY = "9lOt4jN32VIIkEOMbaZAA2hZMasVaQWc7GKtKIa9"
-
+    const val API_KEY = BuildConfig.API_KEY
     const val TEST_START_DATE = "2015-09-07"
     const val TEST_END_DATE = "2015-09-08"
 }
